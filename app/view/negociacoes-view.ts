e416090a82558d250bab03d74fe04031ed82c492
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-05-28 23:44:01 -0300

    Unindo view e modelo

--- app/view/negociacoes-view.ts
+++ app/view/negociacoes-view.ts
@@ -1,3 +1,4 @@
+import { Negociacoes } from './../models/negociacoes.js';
 export class NegociacoesView {
 
     private elemento: HTMLElement;
@@ -6,7 +7,7 @@ export class NegociacoesView {
         this.elemento = document.querySelector(seletor);
     }
 
-    template(): string {
+    template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -17,13 +18,24 @@ export class NegociacoesView {
                 </tr>
             </thead>
             <tbody>
+            ${model.lista().map(negociacao => {
+                return `
+                    <tr>
+                        <td></td>
+                        <td>${ negociacao.quantidade }</td>
+                        <td>${ negociacao.valor }</td>
+                    </tr>
+                `
+            }).join(' ')}
             </tbody>
         </table>
         `;
     }
 
-    update(): void  {
-        this.elemento.innerHTML = this.template()
+    update(model: Negociacoes): void  {
+        const template = this.template(model)
+        console.log(template);
+        this.elemento.innerHTML = template 
     }
 
 }
