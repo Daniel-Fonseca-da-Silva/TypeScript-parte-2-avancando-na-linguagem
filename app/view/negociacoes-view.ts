217bd27c9476f5865c5fde805028275d3a0e6317
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-05-28 22:47:26 -0300

    Integrando nossa view à página

--- app/view/negociacoes-view.ts
+++ app/view/negociacoes-view.ts
@@ -1,15 +1,29 @@
 export class NegociacoesView {
+
+    private elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
     template(): string {
         return `
         <table class="table table-hover table-bordered">
-            <thead
+            <thead>
                 <tr>
                     <th>DATA</th>
                     <th>QUANTIDADE</th>
                     <th>VALOR</th>
                 </tr>
-            ></thead>
+            </thead>
+            <tbody>
+            </tbody>
         </table>
-        `
+        `;
     }
+
+    update(): void  {
+        this.elemento.innerHTML = this.template()
+    }
+
 }
