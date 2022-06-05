69ea1023407804ab67b07ad02484f7b5461624eb
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-06-04 23:51:22 -0300

    Parâmetros opcionais

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,9 +2,14 @@ export class View {
     constructor(seletor, escapar) {
         this.escapar = false;
         this.elemento = document.querySelector(seletor);
+        if (escapar)
+            this.escapar = escapar;
     }
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = template;
     }
 }
